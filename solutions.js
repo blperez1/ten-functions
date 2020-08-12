2b900f5b6cdf4fe8f97f229b747de5de998b1019
Blossom Perez <blossomperez18@gmail.com>
2020-08-12 17:46:41 -0500

    completed functions practice assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,46 @@
  * and
  * concat
  */
+
+function isTrue(input) {
+    return input === true;
+}
+
+function isFalse(input) {
+    return input === false;
+}
+
+function not(input) {
+    return !input;
+}
+
+function addOne(input) {
+    return Number(input) + 1;
+}
+
+function isEven(input) {
+    if (input === false) {
+        return false;
+    }
+    return input % 2 === 0;
+}
+
+function isIdentical(in1, in2) {
+    return in1 === in2;
+}
+
+function isEqual(in1, in2) {
+    return in1 == in2;
+}
+
+function or(in1, in2) {
+    return in1 || in2;
+}
+
+function and(in1, in2) {
+    return in1 && in2;
+}
+
+function concat(in1, in2) {
+    return `${in1}${in2}`;
+}
